8e12b9d3b933bcf3d19f7b51baa7d7bf04b91a3e
Shadowdancer <phantomhive0901@gmail.com>
2026-05-23 17:59:16 +0200

    Pictures added

--- app/src/main/java/com/example/neveranother/MainActivity.kt
+++ app/src/main/java/com/example/neveranother/MainActivity.kt
@@ -6,29 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.neveranother.ui.theme.NeverAnotherTheme
+import com.example.neveranother.ui.theme.homepageUI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NeverAnotherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            homepageUI()
                 }
             }
         }
-    }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
