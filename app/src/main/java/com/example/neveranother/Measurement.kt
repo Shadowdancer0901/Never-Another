b33370daa5b8ed2324e59a7347518efb31b8e245
Christoffer <christofferbzh@gmail.com>
2026-05-25 22:52:55 +0200

    tilføjet Scaffolding

--- app/src/main/java/com/example/neveranother/Measurement.kt
+++ app/src/main/java/com/example/neveranother/Measurement.kt
@@ -15,15 +15,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
@@ -66,193 +73,221 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun App() {
-    val openDialog = remember {mutableIntStateOf(0)}
+    val itemCount = MeasurementpagePictures.size
+    val loops = 1000
+    val virtualCount = itemCount * loops
+    val initialPage = (virtualCount / 2 - ((virtualCount / 2) % itemCount))
+    val pagerState = rememberPagerState(initialPage = initialPage, pageCount = { virtualCount })
+
+    val openDialog = remember { mutableIntStateOf(0) }
     val cContext = LocalContext.current
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-
-    ) {
-        val neverAnotherLogo = painterResource(R.drawable.never_another_logo)
-        Image(
-            painter = neverAnotherLogo,
-            contentDescription = "Never Another Logo"
-        )
-
-        val measurementPic = painterResource(R.drawable.oevre_omkreds_front)
-        Image(
-            painter = measurementPic,
-            contentDescription = "Øvre omkreds Front"
-        )
-
-        Text("Mål", modifier = Modifier.padding(6.dp))
-
-        Row(
-            modifier = Modifier
-                .padding(24.dp)
-        ) {
-
-            //Brugt dette link til at style OutlinedTextField:
-            // https://stackoverflow.com/questions/66453775/how-to-change-the-outline-color-of-outlinedtextfield-from-jetpack-compose
-            OutlinedTextField(
-                state = rememberTextFieldState(),
-                textStyle = TextStyle(fontSize = 10.sp),
-                label = { Text("Øvre omkreds", fontSize = 8.sp) },
-                placeholder = { Text("XX", fontSize = 5.sp, color = Gray) },
-                modifier = Modifier
-                    .size(100.dp, 50.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    unfocusedBorderColor = Salmon,
-                    unfocusedTextColor = Salmon
-                )
-            )
-
-            val firstInfoIcon = painterResource(R.drawable.info_icon)
-            Image(
-                painter = firstInfoIcon,
-                contentDescription = "Information",
-                modifier = Modifier
-                    .padding(top = 20.dp, start = 10.dp, end = 50.dp)
-                    .size(15.dp)
-                    .clickable { openDialog.intValue = 1 },
-                alignment = Alignment.Center
-            )
-
-            OutlinedTextField(
-                state = rememberTextFieldState(),
-                textStyle = TextStyle(fontSize = 10.sp),
-                label = { Text("Nedre omkreds", fontSize = 8.sp) },
-                placeholder = { Text("XX", fontSize = 5.sp, color = Gray) },
-                modifier = Modifier
-                    .size(100.dp, 50.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    unfocusedBorderColor = Salmon,
-                    unfocusedTextColor = Salmon
-                )
-            )
-
-            val secondInfoIcon = painterResource(R.drawable.info_icon)
-            Image(
-                painter = secondInfoIcon,
-                contentDescription = "Information",
-                modifier = Modifier
-                    .padding(top = 20.dp, start = 10.dp)
-                    .size(15.dp)
-                    .clickable { openDialog.intValue = 2 },
-                alignment = Alignment.Center
-            )
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(containerColor = White)
+            { (MeasurementNavbar()) }
         }
-
-        Row(
+    ) { innerPadding ->
+        Column(
             modifier = Modifier
-                .padding(24.dp)
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            OutlinedTextField(
-                state = rememberTextFieldState(),
-                textStyle = TextStyle(fontSize = 10.sp),
-                label = { Text("Nedre omkreds", fontSize = 8.sp) },
-                placeholder = { Text("XX", fontSize = 5.sp, color = Gray) },
-                modifier = Modifier
-                    .size(100.dp, 50.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    unfocusedBorderColor = Salmon,
-                    unfocusedTextColor = Salmon
-                )
-            )
-
-            val thirdInfoIcon = painterResource(R.drawable.info_icon)
+            val neverAnotherLogo = painterResource(R.drawable.never_another_logo)
             Image(
-                painter = thirdInfoIcon,
-                contentDescription = "Information",
-                modifier = Modifier
-                    .padding(top = 20.dp, start = 10.dp, end = 50.dp)
-                    .size(15.dp)
-                    .clickable { openDialog.intValue = 3 },
-                alignment = Alignment.Center
+                painter = neverAnotherLogo,
+                contentDescription = "Never Another Logo"
             )
 
-            OutlinedTextField(
-                state = rememberTextFieldState(),
-                textStyle = TextStyle(fontSize = 10.sp),
-                label = { Text("Nedre omkreds", fontSize = 8.sp) },
-                placeholder = { Text("XX", fontSize = 5.sp, color = Gray) },
+            HorizontalPager(
+                state = pagerState,
                 modifier = Modifier
-                    .size(100.dp, 50.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    unfocusedBorderColor = Salmon,
-                    unfocusedTextColor = Salmon
-                )
-            )
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .padding(vertical = 10.dp, horizontal = 10.dp)
+            ) { i ->
+                val actualIndex = i % itemCount
+                val MkList = MeasurementpagePictures[actualIndex]
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = MkList.imageRes),
+                        contentDescription = MkList.description,
+                        modifier = Modifier
+                            .width(MkList.pictureWidth)
+                            .height(MkList.pictureHeight)
+                    )
+                }
+            }
 
-            val forthInfoIcon = painterResource(R.drawable.info_icon)
-            Image(
-                painter = forthInfoIcon,
-                contentDescription = "Information",
+            Text("Mål", modifier = Modifier.padding(6.dp))
+
+            Row(
                 modifier = Modifier
-                    .padding(top = 20.dp, start = 10.dp)
-                    .size(15.dp)
-                    .clickable { openDialog.intValue = 4 },
-                alignment = Alignment.Center
-            )
-        }
+                    .padding(24.dp)
+            ) {
 
-        when (openDialog.intValue) {
-            1 -> {
-                PopupFunction(
-                    cContext,
-                    "https://neveranother.dk/cdn/shop/videos/c/vp/0e5fa048aa2d4be98a4d6a43dbc8cf1e/0e5fa048aa2d4be98a4d6a43dbc8cf1e.HD-1080p-2.5Mbps-45153518.mp4?v=0"
+                //Brugt dette link til at style OutlinedTextField:
+                // https://stackoverflow.com/questions/66453775/how-to-change-the-outline-color-of-outlinedtextfield-from-jetpack-compose
+                OutlinedTextField(
+                    state = rememberTextFieldState(),
+                    textStyle = TextStyle(fontSize = 10.sp),
+                    label = { Text("Øvre omkreds", fontSize = 8.sp) },
+                    placeholder = { Text("XX", fontSize = 5.sp, color = Gray) },
+                    modifier = Modifier
+                        .size(100.dp, 50.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = Salmon,
+                        unfocusedTextColor = Salmon
+                    )
+                )
+
+                val firstInfoIcon = painterResource(R.drawable.info_icon)
+                Image(
+                    painter = firstInfoIcon,
+                    contentDescription = "Information",
+                    modifier = Modifier
+                        .padding(top = 20.dp, start = 10.dp, end = 50.dp)
+                        .size(15.dp)
+                        .clickable { openDialog.intValue = 1 },
+                    alignment = Alignment.Center
+                )
+
+                OutlinedTextField(
+                    state = rememberTextFieldState(),
+                    textStyle = TextStyle(fontSize = 10.sp),
+                    label = { Text("Nedre omkreds", fontSize = 8.sp) },
+                    placeholder = { Text("XX", fontSize = 5.sp, color = Gray) },
+                    modifier = Modifier
+                        .size(100.dp, 50.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = Salmon,
+                        unfocusedTextColor = Salmon
+                    )
+                )
+
+                val secondInfoIcon = painterResource(R.drawable.info_icon)
+                Image(
+                    painter = secondInfoIcon,
+                    contentDescription = "Information",
+                    modifier = Modifier
+                        .padding(top = 20.dp, start = 10.dp)
+                        .size(15.dp)
+                        .clickable { openDialog.intValue = 2 },
+                    alignment = Alignment.Center
                 )
             }
 
-            2 -> {
-                PopupFunction(
-                    cContext,
-                    "https://neveranother.dk/cdn/shop/videos/c/vp/e1ae0f9b502042e79995107cf269ac08/e1ae0f9b502042e79995107cf269ac08.HD-1080p-2.5Mbps-45153519.mp4?v=0"
+            Row(
+                modifier = Modifier
+                    .padding(24.dp)
+            ) {
+                OutlinedTextField(
+                    state = rememberTextFieldState(),
+                    textStyle = TextStyle(fontSize = 10.sp),
+                    label = { Text("Nedre omkreds", fontSize = 8.sp) },
+                    placeholder = { Text("XX", fontSize = 5.sp, color = Gray) },
+                    modifier = Modifier
+                        .size(100.dp, 50.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = Salmon,
+                        unfocusedTextColor = Salmon
+                    )
+                )
+
+                val thirdInfoIcon = painterResource(R.drawable.info_icon)
+                Image(
+                    painter = thirdInfoIcon,
+                    contentDescription = "Information",
+                    modifier = Modifier
+                        .padding(top = 20.dp, start = 10.dp, end = 50.dp)
+                        .size(15.dp)
+                        .clickable { openDialog.intValue = 3 },
+                    alignment = Alignment.Center
+                )
+
+                OutlinedTextField(
+                    state = rememberTextFieldState(),
+                    textStyle = TextStyle(fontSize = 10.sp),
+                    label = { Text("Nedre omkreds", fontSize = 8.sp) },
+                    placeholder = { Text("XX", fontSize = 5.sp, color = Gray) },
+                    modifier = Modifier
+                        .size(100.dp, 50.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = Salmon,
+                        unfocusedTextColor = Salmon
+                    )
+                )
+
+                val forthInfoIcon = painterResource(R.drawable.info_icon)
+                Image(
+                    painter = forthInfoIcon,
+                    contentDescription = "Information",
+                    modifier = Modifier
+                        .padding(top = 20.dp, start = 10.dp)
+                        .size(15.dp)
+                        .clickable { openDialog.intValue = 4 },
+                    alignment = Alignment.Center
                 )
             }
 
-            3 -> {
-                PopupFunction(
-                    cContext,
-                    "https://neveranother.dk/cdn/shop/videos/c/vp/9750bff2d3954cf49bc0829307df8a5c/9750bff2d3954cf49bc0829307df8a5c.HD-1080p-2.5Mbps-45153520.mp4?v=0"
-                )
-            }
+            when (openDialog.intValue) {
+                1 -> {
+                    PopupFunction(
+                        cContext,
+                        "https://neveranother.dk/cdn/shop/videos/c/vp/0e5fa048aa2d4be98a4d6a43dbc8cf1e/0e5fa048aa2d4be98a4d6a43dbc8cf1e.HD-1080p-2.5Mbps-45153518.mp4?v=0"
+                    )
+                }
 
-            4 -> {
-                PopupFunction(
-                    cContext,
-                    "https://neveranother.dk/cdn/shop/videos/c/vp/15e37945865649c6ad85f73d25baa26e/15e37945865649c6ad85f73d25baa26e.HD-1080p-2.5Mbps-45153521.mp4?v=0"
-                )
-            }
+                2 -> {
+                    PopupFunction(
+                        cContext,
+                        "https://neveranother.dk/cdn/shop/videos/c/vp/e1ae0f9b502042e79995107cf269ac08/e1ae0f9b502042e79995107cf269ac08.HD-1080p-2.5Mbps-45153519.mp4?v=0"
+                    )
+                }
 
-            //Brugt dette link til at lave underline til Text element:
-            // https://developer.android.com/develop/ui/compose/text/style-text
+                3 -> {
+                    PopupFunction(
+                        cContext,
+                        "https://neveranother.dk/cdn/shop/videos/c/vp/9750bff2d3954cf49bc0829307df8a5c/9750bff2d3954cf49bc0829307df8a5c.HD-1080p-2.5Mbps-45153520.mp4?v=0"
+                    )
+                }
 
-            //Brugt dette link til at lave style knappen:
-            // https://kotlinandroid.org/android-jetpack-compose-set-button-background-color/
-            else -> {
-                Text("Usikker på størrelsen?", fontSize = 8.sp)
-                Text("Bestil free fitting", fontSize = 8.sp, textDecoration = TextDecoration.Underline)
+                4 -> {
+                    PopupFunction(
+                        cContext,
+                        "https://neveranother.dk/cdn/shop/videos/c/vp/15e37945865649c6ad85f73d25baa26e/15e37945865649c6ad85f73d25baa26e.HD-1080p-2.5Mbps-45153521.mp4?v=0"
+                    )
+                }
+
+                //Brugt dette link til at lave underline til Text element:
+                // https://developer.android.com/develop/ui/compose/text/style-text
 
                 //Brugt dette link til at lave style knappen:
                 // https://kotlinandroid.org/android-jetpack-compose-set-button-background-color/
-                Button(
-                    onClick = {}, modifier = Modifier
-                        .padding(24.dp)
-                        .size(width = 120.dp, height = 50.dp),
-                    shape = RoundedCornerShape(10.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Salmon)
-                ) {
-                    Text("Bestil")
+                else -> {
+                    Text("Usikker på størrelsen?", fontSize = 8.sp)
+                    Text(
+                        "Bestil free fitting",
+                        fontSize = 8.sp,
+                        textDecoration = TextDecoration.Underline
+                    )
+
+                    //Brugt dette link til at lave style knappen:
+                    // https://kotlinandroid.org/android-jetpack-compose-set-button-background-color/
+                    Button(
+                        onClick = {}, modifier = Modifier
+                            .padding(24.dp)
+                            .size(width = 120.dp, height = 50.dp),
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Salmon)
+                    ) {
+                        Text("Bestil")
+                    }
                 }
-                Column(
-                    modifier = Modifier.fillMaxHeight(),
-                    verticalArrangement = Arrangement.Bottom
-                ) { MeasurementNavbar() }
             }
         }
     }
